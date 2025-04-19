0155dcbee3eec1d99a957c2cab8163df02dc8e22
Sidrim <sidrimthiago@gmail.com>
2025-04-19 15:06:12 -0300

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 const path = require('path');
 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -40,6 +45,7 @@ const nextConfig = {
     // Removemos nodeMiddleware daqui
   },
   webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
