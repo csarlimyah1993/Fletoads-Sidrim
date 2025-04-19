41086d4dfc8f7fb87c55ba88844269ffdfbb25d2
Sidrim <sidrimthiago@gmail.com>
2025-04-19 15:12:18 -0300

    fiz next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,19 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.vercel-storage.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.vercel.app",
-      },
-      {
-        protocol: "https",
-        hostname: "**.blob.vercel-storage.com",
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
+      { protocol: "https", hostname: "**.vercel-storage.com" },
+      { protocol: "https", hostname: "**.vercel.app" },
+      { protocol: "https", hostname: "**.blob.vercel-storage.com" },
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
     ],
     domains: [
       "picsum.photos",
@@ -32,15 +21,13 @@ const nextConfig = {
       'xaywsd0belf9yeug.public.blob.vercel-storage.com',
       'public.blob.vercel-storage.com',
       'vercel-storage.com',
-      'vercel-blob.com'
+      'vercel-blob.com',
     ],
   },
   experimental: {
     serverComponentsExternalPackages: ['mongoose'],
-    // Removemos nodeMiddleware daqui
   },
   webpack: (config, { isServer }) => {
-
     config.resolve.alias['@'] = path.resolve(__dirname);
 
     if (!isServer) {
@@ -54,6 +41,7 @@ const nextConfig = {
         aws4: false,
       };
     }
+
     return config;
   },
 };
